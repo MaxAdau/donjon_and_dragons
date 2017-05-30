dd596cf1982b7b08eae468f26c3c43d0d4fc2b3b
MaxAdau <maxadau@gmail.com>
2017-05-30 18:08:32 +0200

    Adding port in app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 
@@ -7,4 +9,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
