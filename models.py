18ebd5b29518d88c5a29760d18fdb78b1487d049
MaxAdau <maxadau@gmail.com>
2017-05-31 17:16:32 +0200

    Adding join for Character and Class, Adding sqlalchemy imports

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
 import datetime
 
 # Initialize a database object
@@ -34,69 +35,85 @@ class BaseModel(db.Model):
 
 # TODO move it to another file
 class Character(BaseModel, db.Model):
-    """Model for the stations table"""
+    """Model for the character table"""
     __tablename__ = 'chars'
 
     # incremental ID
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
     # About the character
-    player_name = db.Column(db.String)
-    name = db.Column(db.String)
-    age = db.Column(db.Integer)
-    level = db.Column(db.Integer)
+    player_name = Column(String)
+    name = Column(String)
+    age = Column(Integer)
+    level = Column(Integer)
+    description = Column(String)
 
     # TODO change it to custom type
-    race = db.Column(db.String)
-    klass = db.Column(db.String)
+    # http://docs.sqlalchemy.org/en/latest/orm/join_conditions.html
+    race_id = Column(Integer, ForeignKey("races.id"))
+    race = db.relationship("Address", foreign_keys=[race_id])
+    klass = Column(String)
 
     # Character carac
     # TODO put it in a custom type
-    str = db.Column(db.Integer)
-    dex = db.Column(db.Integer)
-    con = db.Column(db.Integer)
-    int = db.Column(db.Integer)
-    wis = db.Column(db.Integer)
-    cha = db.Column(db.Integer)
+    str = Column(Integer)
+    dex = Column(Integer)
+    con = Column(Integer)
+    int = Column(Integer)
+    wis = Column(Integer)
+    cha = Column(Integer)
 
     # Base attack Bonus
-    BAB = db.Column(db.Integer)
+    BAB = Column(Integer)
 
     # Armor Class
-    AC_natural = db.Column(db.Integer)
-    magic_resist = db.Column(db.Integer)
+    AC_natural = Column(Integer)
+    magic_resist = Column(Integer)
     # TODO calculate it instead of hard code it
-    AC = db.Column(db.Integer)
-    AC_flatfooted = db.Column(db.Integer)
-    AC_touch = db.Column(db.Integer)
+    AC = Column(Integer)
+    AC_flatfooted = Column(Integer)
+    AC_touch = Column(Integer)
 
     # Size and speed
-    size = db.Column(db.Integer)
-    initiative = db.Column(db.Integer)
+    size = Column(Integer)
+    initiative = Column(Integer)
 
     # TODO Should be calculated
-    speed = db.Column(db.Integer)
+    speed = Column(Integer)
 
     # Saving throw
-    SAVE_ref = db.Column(db.Integer)
-    SAVE_for = db.Column(db.Integer)
-    SAVE_will = db.Column(db.Integer)
+    SAVE_ref = Column(Integer)
+    SAVE_for = Column(Integer)
+    SAVE_will = Column(Integer)
 
     # Weapons # TODO Change type to weapon type
-    right_hand = db.Column(db.String)
-    left_hand = db.Column(db.String)
+    right_hand = Column(String)
+    left_hand = Column(String)
 
     # Armor # TODO Change type armor/shield type
-    armor = db.Column(db.String)
-    shield = db.Column(db.String)
+    armor = Column(String)
+    shield = Column(String)
 
     # Stuff # TODO change it to list os equipement type
-    head_protection = db.Column(db.String)
-    hand_protection = db.Column(db.String)
-    foot_protection = db.Column(db.String)
-    head_protection = db.Column(db.String)
+    head_protection = Column(String)
+    hand_protection = Column(String)
+    foot_protection = Column(String)
+    ring_1 = Column(String)
 
 
+class Race(BaseModel, db.Model):
+    """Model for the race table"""
+    __tablename__ = 'races'
+
+    # incremental ID
+    id = Column(Integer, primary_key=True)
+
+    # About the race
+    name = Column(String)
+    description = Column(String)
+
+    # TODO move it to an ENUM type
+    size = Column(String)
 
 
 
