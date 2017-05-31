f4a270403da49b7fefc0d9520e344f15b71ab859
MaxAdau <maxadau@gmail.com>
2017-05-31 13:48:14 +0200

    Adding baseModel and Char class definition. Updating requirements

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 # Initialize a database object
 # Could be moved to a model.py file
@@ -10,11 +11,31 @@ db = SQLAlchemy()
 class BaseModel(db.Model):
     """Base data model for all objects"""
     __abstract__ = True
-    # define here __repr__ and json methods or any common method
-    # that you need for all your models
 
-class YourModel(BaseModel):
-    """model for one of your table"""
-    __tablename__ = 'my_table'
-    # define your model
+    def __init__(self, *args):
+        super().__init__(*args)
 
+    def __repr__(self):
+        """Define a base way to print models"""
+        return '%s(%s)' % (self.__class__.__name__, {
+            column: value
+            for column, value in self._to_dict().items()
+        })
+
+    def json(self):
+        """
+                Define a base way to jsonify models, dealing with datetime objects
+        """
+        return {
+            column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
+            for column, value in self._to_dict().items()
+        }
+
+
+class Char(BaseModel, db.Model):
+    """Model for the stations table"""
+    __tablename__ = 'chars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
